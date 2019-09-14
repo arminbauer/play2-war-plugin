903e717e8850229721efa0328c0abe47d7c91327
Axel Kämpfe <axel.kaempfe@idnow.de>
2019-09-14 18:41:01 +0200

    remove pom deploy

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -63,7 +63,7 @@ object Build extends Build {
   //
   lazy val play2WarPlugin = Project(id = "play2-war-plugin",
     base = file("plugin"),
-    settings = commonSettings ++ ivySettings ++ mavenSettings ++ Seq(
+    settings = commonSettings ++ ivySettings ++ Seq(
       publishArtifact := true,
       scalaVersion := buildScalaVersionForSbt,
       scalaBinaryVersion := buildScalaVersionForSbtBinaryCompatible,
